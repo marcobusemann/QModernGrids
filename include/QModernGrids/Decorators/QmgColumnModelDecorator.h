50dbe8efccd35820b4f2d3834e6a2cf94f78a83e
Marco Busemann <marco.busemann@live.com>
2018-01-09 08:31:29 +0100

    The column model decorator now forward dataChanged
    It spans the bottom index to the full row to ensure corrent view updates.

--- include/QModernGrids/Decorators/QmgColumnModelDecorator.h
+++ include/QModernGrids/Decorators/QmgColumnModelDecorator.h
@@ -6,11 +6,17 @@
 
 class QMODERNGRIDS_LIBRARY_API QmgColumnModelDecorator : public QIdentityProxyModel
 {
+   Q_OBJECT
+
 public:
    QmgColumnModelDecorator(int columns, QObject *parent = nullptr);
+   virtual void setSourceModel(QAbstractItemModel *sourceModel) override;
    virtual int	columnCount(const QModelIndex & = QModelIndex()) const override;
-   virtual bool hasChildren(const QModelIndex &parent) const;
+   virtual bool hasChildren(const QModelIndex &parent) const override;
 
 private:
    int m_columns;
+
+private slots:
+   void forwardDataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight);
 };
--- src/Decorators/QmgColumnModelDecorator.cpp
+++ src/Decorators/QmgColumnModelDecorator.cpp
@@ -4,7 +4,19 @@
 QmgColumnModelDecorator::QmgColumnModelDecorator(int columns, QObject *parent)
    : QIdentityProxyModel(parent)
    , m_columns(columns)
-{}
+{
+}
+
+void QmgColumnModelDecorator::setSourceModel(QAbstractItemModel *sourceModel)
+{
+   if (this->sourceModel())
+      disconnect(this->sourceModel());
+
+   QIdentityProxyModel::setSourceModel(sourceModel);
+
+   if (this->sourceModel())
+      connect(this->sourceModel(), SIGNAL(dataChanged(QModelIndex, QModelIndex)), this, SLOT(forwardDataChanged(QModelIndex, QModelIndex)));
+}
 
 int QmgColumnModelDecorator::columnCount(const QModelIndex &index) const
 {
@@ -15,4 +27,10 @@ int QmgColumnModelDecorator::columnCount(const QModelIndex &index) const
 bool QmgColumnModelDecorator::hasChildren(const QModelIndex &parent) const
 {
    return (rowCount(parent) > 0) && (columnCount(parent) > 0);
+}
+
+void QmgColumnModelDecorator::forwardDataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight)
+{
+   const auto realBottomRight = bottomRight.sibling(bottomRight.row(), columnCount() - 1);
+   emit dataChanged(mapFromSource(topLeft), mapFromSource(realBottomRight));
 }
